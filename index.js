facd690c573585568d9adc3db86df00934a96189
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-29 17:33:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ function checkCollision(rock) {
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
-    const dodgerRightEdge = dodgerLeftEdge + parseInt(DODGER.offsetWidth);
+    const dodgerRightEdge = dodgerLeftEdge + DODGER.offsetWidth;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
-    const rockRightEdge = rockLeftEdge + parseInt(rock.offsetWidth);
+    const rockRightEdge = rockLeftEdge + rock.offsetWidth;
 
     if ((rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
         (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
